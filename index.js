da1e22600c3b8a1d23791cf5ed002cef5348e33b
RameenKhannnn <rameenkhan8787@gmail.com>
2024-07-01 23:10:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var numberOfStars = Math.round(scoreAvg);
+scoreAvg = Math.round(scoreAvg);
+var scoreAvg = Math.round(.0678437);
+var scoreAvg = Math.ceil(.000001);
+var scoreAvg = Math.floor(.999999);
+var randomNumber = Math.random();
+ var bigDecimal = Math.random();
+ var improvedNum = (bigDecimal * 6) + 1;
+ var numberOfStars = Math.floor(improvedNum);
+ var currentAge = prompt("Enter your age.");
+ var currentAge = prompt("Enter your age.");
+ var yearsEligibleToVote = currentAge - 18;
+ var profit = "200" - "150";
+ var profit = "200" - "duck";
+ var result = "200" + 150;
+ var currentAge = prompt("Enter your age.");
+ var qualifyingAge = currentAge + 1;
+ var currentAge = prompt("Enter your age.");
+ var qualifyingAge = parseInt(currentAge) + 1;
+ var myInteger = parseInt("1.9999");
+ var myFractional = parseFloat("1.9999");
+  var integerString = "24"
+  var num = Number(integerString);
+  var floatingNumString = "24.9876";
+  var num = Number(floatingNumString);
+  var numberAsNumber = 1234;
+  var numberAsString = numberAsNumber.toString();
+  var total = price + (price * taxRate);
+  var prettyTotal = total.toFixed(2);
+  var currencyTotal = "$" + prettyTotal;
+  var prettyTotal = total.toFixed();
+ var str = num.toString();
+if (str.charAt(str.length - 1) === "5") {
+    str = str.slice(0, str.length - 1) + "6";
+  }
+ num = Number(str);
+  prettyNum = num.toFixed(2);
+
+
